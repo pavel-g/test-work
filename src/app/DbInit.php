70a5cf82bb9a67920cbd4a47aafb9ce4496a8cc4
Gnedov Pavel <pavel@gnedov.info>
2017-01-15 12:40:46 +0700

    Добавлена функция очистки таблиц

--- src/app/DbInit.php
+++ src/app/DbInit.php
@@ -49,4 +49,20 @@ class DbInit {
 		return ((boolean) ($results['log'] !== false && $results['browsers'] !== false));
 	}
 	
+	/**
+	 * Очистка таблиц
+	 * @return boolean
+	 */
+	public static function clearTables() {
+		$db = Db::getInstance();
+		$queries = array(
+			'log' => "DELETE FROM log",
+			'browsers' => "DELETE FROM browsers"
+		);
+		$results = array();
+		$results['log'] = $db->query($queries['log']);
+		$results['browsers'] = $db->query($queries['browsers']);
+		return ((boolean) $results['log'] !== false && $results['browsers'] !== false);
+	}
+	
 }
